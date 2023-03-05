e833203c8dac68b7ebcb747564994d880be64437
Roman Demianchuk <demianchuk.ro@gmail.com>
2023-03-05 14:32:24 +0200

    testing validation

--- Controllers/ApiControllerBase.cs
+++ Controllers/ApiControllerBase.cs
@@ -1,8 +1,11 @@
+using System.Net.Mime;
 namespace NetCoreCourse.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
+[Produces(MediaTypeNames.Application.Json)]
+[Consumes(MediaTypeNames.Application.Json)]
 [Route("[controller]s")]
 public class ApiControllerBase : ControllerBase 
 {
--- DTOs/CourseDTO.cs
+++ DTOs/CourseDTO.cs
@@ -1,10 +1,26 @@
 namespace NetCoreCourse.DTOs;
 
+using System.ComponentModel.DataAnnotations;
 using NetCoreCourse.Models;
+using NetCoreCourse.Common;
+using System.Collections.Generic;
 
-public class CourseDTO
+public class CourseDTO : IValidatableObject
 {
+    // [MinLength(5, ErrorMessage = "Name is too short, 5 chars minimum")]
+    [StringLength(200, MinimumLength = 5, ErrorMessage = "max 200, min 5.")]
     public string Name {get; set;}
+
+    [CourseStartDate(ErrorMessage = "Start Date should be same year.")]
     public DateTime StartDate {get; set;}
+
     public Course.CourseStatus Status {get; set;}
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if(StartDate < DateTime.Now && Status == Course.CourseStatus.NotStarted)
+        {
+            yield return new ValidationResult("Wrong Status. Course should've start already.");
+        }
+    }
 }
